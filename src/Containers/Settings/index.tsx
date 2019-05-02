1853e71502831fed9956842f1dde7e875f58fc71
HegelPro <leo.palkin@gmail.com>
2019-05-03 00:07:09 +0400

    delete fieldSettings from fieldState from

--- src/Containers/Settings/index.tsx
+++ src/Containers/Settings/index.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
-import FormGroup from '@material-ui/core/FormGroup'
-import Button from '@material-ui/core/Button'
 import { useDispatch, useMappedState } from 'redux-react-hook'
+import Button from '@material-ui/core/Button'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormGroup from '@material-ui/core/FormGroup'
+import TextField from '@material-ui/core/TextField'
 
 import history from '../../setup/history'
-import { setFieldSettingsAction } from './actions'
-import { avaibleSizesForField } from './config'
-import TextField from '@material-ui/core/TextField'
-import MenuItem from '@material-ui/core/MenuItem'
 import { RootState } from '../../store/types'
+
 import { selectSettings } from './selectors'
+import { avaibleSizesForField } from './config'
+import { setFieldSettingsAction } from './actions'
 
 const mapState = (state: RootState) => ({
   fieldSettings: selectSettings(state),
@@ -27,13 +28,13 @@ const Settings = () => {
         select
         title='rows'
         value={rowsInputValue}
-        onChange={({target: { value }}) => setRowsInputValue(+value)}
+        onChange={({ target: { value } }) => setRowsInputValue(+value)}
       >{menuItemsForSelectors}</TextField>
       <TextField
         select
         title='columns'
         value={columnsInputValue}
-        onChange={({target: { value }}) => setColumnsInputValue(+value)}
+        onChange={({ target: { value } }) => setColumnsInputValue(+value)}
       >{menuItemsForSelectors}</TextField>
       <Button
         onClick={() => {
--- src/models/fieldState/index.ts
+++ src/models/fieldState/index.ts
@@ -1,7 +1,6 @@
 import { Record, Map } from 'immutable'
 
 import { FieldRecord } from '../../models/field'
-import { FieldSettingsRecord } from '../fieldSettings'
 
 import { IFieldState } from './types'
 
@@ -9,7 +8,6 @@ const defaultFieldState: IFieldState = {
   current: new FieldRecord(),
   previous: new FieldRecord(),
   records: Map(),
-  settings: new FieldSettingsRecord(),
 }
 
 export class FieldStateRecord extends Record<IFieldState>(defaultFieldState) {}
